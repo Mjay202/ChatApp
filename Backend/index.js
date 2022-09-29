4b94afd22421d60acb32a0a37528af4cebcead6a
Hammed Mujeeb <hamkamolujide@gmail.com>
2022-09-29 11:05:07 +0100

    added dotenv backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,27 @@
 const express = require ("express")
 const app = express();
 
+// dotenv
+require ("dotenv").config()
+
+
+// set cors
+const cors = require('cors')
+app.use(cors())
+
+// set up moongoose
+
+
+const connection = app.listen(PORT, ()=> {
+    console.log("The server has started successfully at PORT " + PORT)
+})
+
+// require and set up socket.io
+
+const socketServer = require ("socket.io")
+const io = socketServer(connection, {
+    cors : {origin: "*"}
+
+})
+
+io.on('connection')
